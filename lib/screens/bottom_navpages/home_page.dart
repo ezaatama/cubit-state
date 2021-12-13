706ef8ab78aa8025814b23f3c1cfebb8319a2c67
reza putra <ezatama33@gmail.com>
2021-12-13 13:09:46 +0700

    Slicing Commit

---
+++ lib/screens/bottom_navpages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({ Key? key }) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              children: [
+                const Icon(Icons.menu, size: 30, color: Colors.black54),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5)
+                  )
+                )
+              ],
+            )
+          ),
+          const SizedBox(height: 40,),
+          AppLargeText(text: "Discover")
+        ],
+      ),
+    );
+  }
+}
